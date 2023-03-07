3c6e909d002cc930c085f1ce4289ac4c260b377b
unknown <nordinzakaria@gmail.com>
2023-03-07 08:49:30 +0100

    Database class more or less restructured

--- src/main/java/com/example/mathtuitioncenter/Database.java
+++ src/main/java/com/example/mathtuitioncenter/Database.java
@@ -8,6 +8,7 @@ public class Database {
     private String db;
     private String username;
     private String passwd;
+    private Connection con;
 
     public Database(String url, String port, String db,
                     String username, String passwd) {
@@ -22,33 +23,30 @@ public class Database {
     public void connect() {
                 // change string to attribute
         try {
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://db4free.net:3306/oopjan23utp",
-                    "nordin", "n00Pdinutp");
+            con = DriverManager.getConnection(
+                this.url + ":" +this.port+"/"+this.db,
+                    this.username, this.passwd);
         }
         catch(Exception e){ System.out.println(e);}
 
     }
 
     public void runQuery(String query) {
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from Student");
+            while (rs.next())
+                System.out.println(rs.getString(1));
+        }
+        catch(Exception e){ System.out.println(e);}
 
     }
 
-
-    public static void main(String args[]){
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://db4free.net:3306/oopjan23utp",
-                    "nordin","n00Pdinutp");
-//here sonoo is database name, root is username and password
-            Statement stmt=con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from Student");
-            while(rs.next())
-                System.out.println(rs.getString(1));
+    public void close() {
+        try {
             con.close();
-        }catch(Exception e){ System.out.println(e);}
+        }
+        catch(Exception e){ System.out.println(e);}
     }
 
 }
