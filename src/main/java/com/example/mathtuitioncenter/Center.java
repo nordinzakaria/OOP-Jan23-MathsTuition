f41726c61c43ca8f867a9efd2993e8d27924a8fe
unknown <nordinzakaria@gmail.com>
2023-03-14 07:57:02 +0100

    added code to Center

--- src/main/java/com/example/mathtuitioncenter/Center.java
+++ src/main/java/com/example/mathtuitioncenter/Center.java
@@ -1,8 +1,28 @@
 package com.example.mathtuitioncenter;
 
+import java.util.*;
+
 public class Center {
     // Center has list(s) of teachers, and student batches
     // there should be method for add and info display
-    
+    private ArrayList<RegularTeacher> rts = new ArrayList<RegularTeacher>();
+    private ArrayList<FreelanceTeacher> fts = new ArrayList<FreelanceTeacher>();
+    private ArrayList<StudentBatch> sbs = new ArrayList<StudentBatch>();
+
+    public void addFTeacher(FreelanceTeacher ft) {
+
+    }
+
+    public void addRTeacher(RegularTeacher rt) {
+
+    }
+
+    public void addStudentBatch(StudentBatch sb) {
+        
+    }
+
+    public static void main(String args[]) {
+
+    }
 
 }
